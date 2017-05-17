64b9593a6beded918398178d3cec38a48afbbd15
supernoir <kawohl.bentodesign@gmail.com>
2017-05-17 16:49:12 +0200

    passes values to components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,9 @@ import { MailService } from './mail.service';
 @Component({
   selector: 'app-root',
   template: `<div>
-    <app-simple-form>
+    <app-simple-form
+      *ngFor="let message of mail.messages"
+      [message]="message">
     </app-simple-form>
     <ul>
       <li *ngFor="let message of mail.messages">
--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-simple-form',
   template: `
     <div>
+    {{message}}
     <input #myInput type="text">
     <button (click)="onClick($event, myInput.value)">Klicka Mig</button>
     </div>
@@ -11,6 +12,8 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class SimpleFormComponent implements OnInit {
+
+  @Input() message;
 
   onClick(event, value) {
     console.log(event, `Jag hittade det med ${value} som value`)
